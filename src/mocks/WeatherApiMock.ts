567242f84a7e91d8b66f79df9283a89a656da0c6
Alex Larragueta <alex.larragueta@gmail.com>
2024-05-16 10:56:13 -0300

    fix: build errors

--- src/mocks/WeatherApiMock.ts
+++ src/mocks/WeatherApiMock.ts
@@ -1,21 +1,25 @@
-import { CityResult, DetailedWeather, BasicWeather } from "@/types";
+import { CityResultDTO, DetailedWeather, BasicWeather } from "@/types";
 import {
   cityResults,
   exampleDetailedCity,
   exampleSelectedCity,
 } from "./weatherResponse";
+import {
+  formatBasicWeather,
+  formatDetailedWeather,
+} from "@/service/weatherApi.utils";
 
 const LATENCY_MS = 300;
 
 let getCitiesDebounceTimer: ReturnType<typeof setTimeout> | null = null;
 
 class WeatherApiMock {
-  static getCities = async (searchTerm: string): Promise<CityResult[]> => {
+  static getCities = async (searchTerm: string): Promise<CityResultDTO[]> => {
     if (getCitiesDebounceTimer !== null) {
       clearTimeout(getCitiesDebounceTimer);
     }
 
-    const results = await new Promise<CityResult[]>((resolve) => {
+    const results = await new Promise<CityResultDTO[]>((resolve) => {
       getCitiesDebounceTimer = setTimeout(() => {
         const cityList = cityResults.filter((city) =>
           city.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -30,17 +34,9 @@ class WeatherApiMock {
   static getWeather = async (cityId: string) => {
     const results = await new Promise<BasicWeather>((resolve) => {
       setTimeout(() => {
-        const selectedCity = {
-          id: cityId,
-          name: exampleSelectedCity.location.name,
-          temperature: exampleSelectedCity.current.temp_c.toString(),
-          weatherDescription: exampleSelectedCity.current.condition.text,
-          humidity: exampleSelectedCity.current.humidity.toString(),
-          windVelocity: exampleSelectedCity.current.wind_kph.toString(),
-          climateIcon: exampleSelectedCity.current.condition.icon,
-        };
+        const basicWeather = formatBasicWeather(exampleSelectedCity);
 
-        resolve(selectedCity);
+        resolve(basicWeather);
       }, LATENCY_MS);
     });
 
@@ -50,32 +46,8 @@ class WeatherApiMock {
   static getDetailedWeather = async (cityId: string, _days: number) => {
     console.log("exampleDetailedCity.forecast: ", exampleDetailedCity.forecast);
     const results = await new Promise<DetailedWeather>((resolve) => {
-      const forecast = exampleDetailedCity.forecast;
       setTimeout(() => {
-        const detailedCity = {
-          id: cityId,
-          name: exampleDetailedCity.location.name,
-          temperature: exampleDetailedCity.current.temp_c.toString(),
-          weatherDescription: exampleDetailedCity.current.condition.text,
-          humidity: exampleDetailedCity.current.humidity.toString(),
-          windVelocity: exampleDetailedCity.current.wind_kph.toString(),
-          climateIcon: exampleDetailedCity.current.condition.icon,
-          forecast: forecast.forecastday.map((forecastday, index) => {
-            const { date, day } = forecastday;
-            const { avgtemp_c, avghumidity, maxwind_mph, condition } = day;
-
-            return {
-              id: index.toString(),
-              date: date,
-              name: exampleDetailedCity.location.name,
-              temperature: avgtemp_c.toString(),
-              weatherDescription: "",
-              humidity: avghumidity.toString(),
-              windVelocity: maxwind_mph.toString(),
-              climateIcon: condition.icon,
-            };
-          }),
-        };
+        const detailedCity = formatDetailedWeather(cityId, exampleDetailedCity);
         resolve(detailedCity);
       }, LATENCY_MS);
     });
@@ -83,31 +55,12 @@ class WeatherApiMock {
     return results;
   };
 
-  static getFavorites = async (cityId: string) => {
+  static getFavorites = async (cityIds: string[]) => {
     const results = await new Promise<BasicWeather[]>((resolve) => {
       setTimeout(() => {
-        const selectedCities = [
-          {
-            id: cityId,
-            name: exampleSelectedCity.location.name,
-            temperature: exampleSelectedCity.current.temp_c.toString(),
-            weatherDescription: exampleSelectedCity.current.condition.text,
-            humidity: exampleSelectedCity.current.humidity.toString(),
-            windVelocity: exampleSelectedCity.current.wind_kph.toString(),
-            climateIcon: exampleSelectedCity.current.condition.icon,
-          },
-          {
-            id: cityId + 1,
-            name: exampleSelectedCity.location.name,
-            temperature: exampleSelectedCity.current.temp_c.toString(),
-            weatherDescription: exampleSelectedCity.current.condition.text,
-            humidity: exampleSelectedCity.current.humidity.toString(),
-            windVelocity: exampleSelectedCity.current.wind_kph.toString(),
-            climateIcon: exampleSelectedCity.current.condition.icon,
-          },
-        ];
+        const selectedCities = cityIds.map((cityId) => this.getWeather(cityId));
 
-        resolve(selectedCities);
+        resolve(Promise.all(selectedCities));
       }, LATENCY_MS);
     });
 
