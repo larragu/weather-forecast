8d9010a12125da21275a15b6526ac36fe59edc59
Alex Larragueta <alex.larragueta@gmail.com>
2024-05-16 23:33:44 -0300

    fix: move down loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { CircularProgress, Box } from "@mui/material";
 
 const Loading = () => {
   return (
-    <Box display="flex" width="100%" justifyContent="center">
+    <Box display="flex" width="100%" justifyContent="center" marginTop={4}>
       <CircularProgress size={100} />
     </Box>
   );
